8487e7d17147cd85294c85d3cfaab0d20dba7e5e
Alexander Tomelovich <tomelovichs@gmail.com>
2024-01-08 14:44:13 +0300

    Проект разбит на отдельные  модули

--- js/script.js
+++ js/script.js
@@ -1,21 +1,29 @@
+import tabs from './modules/tabs';
+import modal from './modules/modal';
+import timer from './modules/timer';
+import cards from './modules/cards';
+import calc from './modules/calc';
+import forms from './modules/forms';
+import slider from './modules/slider';
+import { openModal } from './modules/modal';
+
 window.addEventListener('DOMContentLoaded', () => {
-    const tabs = require('./modules/tabs'),
-        modal = require('./modules/modal'),
-        timer = require('./modules/timer'),
-        cards = require('./modules/cards'),
-        calc = require('./modules/calc'),
-        forms = require('./modules/forms'),
-        slider = require('./modules/slider');
+    const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 300000);
 
-
-    tabs();
-    modal();
-    timer();
+    tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+    modal('[data-modal]', '.modal', modalTimerId);
+    timer('.timer', '2024-01-14');
     cards();
     calc();
-    forms();
-    slider();
-
-
-
+    forms('form', modalTimerId);
+    slider({
+        container: '.offer__slider',
+        nextArrow: '.offer__slider-next',
+        prevArrow: '.offer__slider-prev',
+        totalCounter: '#total',
+        currentCounter: '#current',
+        slide: '.offer__slide',
+        wrapper: '.offer__slider-wrapper',
+        field:'.offer__slider-inner' 
+    });
 });
